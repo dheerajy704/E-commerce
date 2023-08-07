93cbed17415770f68c7a90db7ce94dea1b759637
dheeraj <dheerajkumar@techis.io>
2023-08-07 12:22:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import CartPage from './pages/CartPage';
+
 
 const router = createBrowserRouter([
   {
@@ -23,6 +25,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
   },
 ]);
 function App() {
